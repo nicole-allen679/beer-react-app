e535d57733f70b2f3471e195bd62d5614d9a6098
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-04 11:31:40 -0400

    added controlled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Beers from "./Components/Beers"
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('')
   return (
     <Router>
     <div className="App">
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </form>
       <header className="App-header">
       <h1>ALL THE BEST BEERS</h1> 
       </header>
